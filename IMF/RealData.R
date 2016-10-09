518047ef667da8e9b3e8f9d20271a96c3023d1eb
RobHayward <robohay@gmail.com>
2016-10-09 09:12:08 +0100

    Working on IMF real data video

---
+++ IMF/RealData.R
@@ -0,0 +1,31 @@
+# Session 6 looking ar real world data
+library(forecast)
+da <- read.csv('Data/Module3_data/PE_Ratios.csv')
+head(da)
+# create a date object. 
+da$date <- as.Date(da[,1], format = '%Y-%m-%d')
+plot(da$date, da$pe_usa, type = 'l')
+daSA <- subset(da, da$date >= '1995-09-01', select = c(date, pe_saf))
+head(daSA)
+plot(daSA, type = 'l', main = "South African PE ratio")
+# Looks stationary
+par(mfrow = c(2, 1))
+print(acf(daSA$pe_saf))
+pacf(daSA$pe_saf)
+# Suggestion is AR4? 
+eqAR4 <- arima(daSA$pe_saf, order = c(4, 0, 0))
+print(eqAR4)
+acf(eqAR4$residuals)
+pacf(eqAR4$residuals)
+eqAR6 <- arima(daSA$pe_saf, order = c(6, 0, 0))
+print(eqAR6)
+acf(eqAR6$residuals)
+pacf(eqAR6$residuals)
+eqARMA61 <- arima(daSA$pe_saf, order = c(6, 0, 1))
+print(eqARMA61)
+# The AR4 has the lowest AIC and SBC. 
+eq <- auto.arima(daSA$pe_saf)
+summary(eq)
+# Now looks like ARMA(2, 1)
+acf(eq$residuals)
+pacf(eq$residuals)
